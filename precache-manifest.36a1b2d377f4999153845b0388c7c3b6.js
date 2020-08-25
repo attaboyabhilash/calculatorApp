f0590b32c52b945b6633b31e80ec801b3aaf1baf
Abhilash Negi <tiger.abhi.3609@gmail.com>
2020-08-25 20:39:18 +0530

    Updates

---
+++ precache-manifest.36a1b2d377f4999153845b0388c7c3b6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fde9725f8e8bc1e888f95998b0c7c05f",
+    "url": "/calculatorApp/index.html"
+  },
+  {
+    "revision": "27cf8f0d5c6b0cbcfcfa",
+    "url": "/calculatorApp/static/css/main.81eccd2e.chunk.css"
+  },
+  {
+    "revision": "c93ca39909f3bcbc4a8a",
+    "url": "/calculatorApp/static/js/2.7c25b403.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calculatorApp/static/js/2.7c25b403.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27cf8f0d5c6b0cbcfcfa",
+    "url": "/calculatorApp/static/js/main.1ba60e81.chunk.js"
+  },
+  {
+    "revision": "39ba6d636b76d601456e",
+    "url": "/calculatorApp/static/js/runtime-main.5368a8e8.js"
+  },
+  {
+    "revision": "a4e328cfaca98b713cba8ea431b9f7e0",
+    "url": "/calculatorApp/static/media/myMainLogoWithBlackText.a4e328cf.svg"
+  }
+]);
